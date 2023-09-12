cc6c780ed73f33c44d3ec1833733d2b9a0748928
mparol <maciej.parol@hadatap.pl>
2023-09-13 00:32:36 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 					priority
 				/>
 			</div>
-
+<div className="font-bold text-5xl">TEST</div>
 			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
 				<a
 					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
