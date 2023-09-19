b461edaf3ffe51fc4d8f3f861a32d356c9680f77
mparol <maciej.parol@hadatap.pl>
2023-09-20 01:51:08 +0200

    m1z1: dodane data-testid

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,7 +3,7 @@ import { ProductListitem } from "@ui/molecules/ProductListItem";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => {
 	return (
-		<ul className="grid min-w-max grid-cols-4 gap-12">
+		<ul className="grid min-w-max grid-cols-4 gap-12" data-testid="products-list">
 			{products.map((product) => (
 				<ProductListitem key={product.id} product={product} />
 			))}
