644552c26877d9e72f7a0cac1635493b569d8d1b
mparol <maciej.parol@hadatap.pl>
2023-10-16 00:46:14 +0200

    cofniete wersje paczek z typami react ze wzgl na bledy w typowaniu eksperymentalnych

--- src/ui/atoms/SubmitButton.tsx
+++ src/ui/atoms/SubmitButton.tsx
@@ -1,10 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client";
 
 // to jest osobny komponent kliencki bo ponoc tak musi byc zeby dzialalo pending z useFormStatus. musi byc wewnatrz <form>
 
-// @ts-expect-error chyba nie ma jeszcze tyou w @types/react-dom dla experimental_useFormStatus, ale ogolnie to dziala
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import type { ComponentProps, PropsWithChildren } from "react";
 
